62c6e13cdb8ef9d4e07cb65442e7a76f981b56a0
Alberto Kitumba <alberkitumba@gmail.com>
2024-08-15 17:52:26 +0100

    feat: add notification test and replace healper

---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Você recebeu uma nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'id-do-recipient',
+      readAt: null,
+    });
+
+    expect(notification).toBeTruthy();
+  });
+});
