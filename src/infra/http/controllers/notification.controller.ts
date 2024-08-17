458142fa6cfe1fe8cf0b405fb7b2b3e7400b41af
Alberto Kitumba <alberkitumba@gmail.com>
2024-08-17 11:26:40 +0100

    feat: add notification view model

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from '../../../application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-model/notification-view-model';
 
 @Controller('notifications')
 export class NotificationController {
@@ -16,6 +17,8 @@ export class NotificationController {
       content,
     });
 
-    return { notification };
+    return {
+      notification: NotificationViewModel.toHTTP(notification),
+    };
   }
 }
