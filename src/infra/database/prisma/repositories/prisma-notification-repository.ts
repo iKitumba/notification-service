638f439a787638ebb224eb44d3c42e3726f5c5e4
Alberto Kitumba <alberkitumba@gmail.com>
2024-08-17 18:27:01 +0100

    feat: implement all methods on prisma notifications repository

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -7,9 +7,6 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class PrismaNotificationRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
-  async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
-  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
@@ -20,6 +17,39 @@ export class PrismaNotificationRepository implements NotificationRepository {
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prismaService.notification.update({
+      where: { id: notification.id },
+      data: raw,
+    });
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prismaService.notification.count({
+      where: { recipientId },
+    });
+
+    return count;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prismaService.notification.findMany({
+      where: { recipientId },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prismaService.notification.findUnique({
+      where: { id: notificationId },
+    });
+
+    if (!notification) {
+      return null;
+    }
+
+    return PrismaNotificationMapper.toDomain(notification);
   }
 }
